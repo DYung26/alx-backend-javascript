28ea038a1a3895bb77841361103703579bae8441
DYuŋ <80885248+DYung26@users.noreply.github.com>
2024-10-01 05:33:07 +0100

    Update `9-getFullBudget.js`

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -2,7 +2,6 @@ import getBudgetObject from './7-getBudgetObject.js';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
-
   return fullBudget = {
     ...budget,
     getIncomeInDollars(income) {
