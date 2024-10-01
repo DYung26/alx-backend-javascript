05320f790ee2c8f066d8ad66b0a7ad59ce708d5e
DYung26 <theadstore1407@gmail.com>
2024-09-30 20:46:51 -0700

    `appendToEachArrayValue` to use `ES6’s` `for...of` operator.

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+export default function appendToEachArrayValue(array, appendString) {
+  let newArray = [];
+
+  for (const value of array) {
+    newArray.push(appendString + value);
+  }
+
+  return newArray;
+}
