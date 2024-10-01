333671129b08076e504bf32183e5cc65ca383ea6
DYung26 <theadstore1407@gmail.com>
2024-09-30 21:23:13 -0700

    fix: rename income parameter to amount in getIncomeInDollars and getIncomeInEuros methods for clarity

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,11 +4,11 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars(income) {
-      return `$${income}`;
+    getIncomeInDollars(amount) {
+      return `$${amount}`;
     },
-    getIncomeInEuros(income) {
-      return `${income} euros`;
+    getIncomeInEuros(amount) {
+      return `${amount} euros`;
     },
   };
 
