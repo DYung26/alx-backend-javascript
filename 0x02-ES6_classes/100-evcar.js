edd553e686b5baf07ba7d9683c11559ad21ff109
DYung26 <theadstore1407@gmail.com>
2024-10-03 21:34:14 -0700

    `EVCar`

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+
+import Car from './100-car.js';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
