60cc08d562cc9fff9fff32c2987b22deb0adbd8d
DYung26 <theadstore1407@gmail.com>
2024-10-03 20:10:10 -0700

    Returns a `Promise` using this prototype function `getResponseFromAPI()`

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const success = true;
+    }, 0);
+  });
+}
+
+export default getResponseFromAPI;
