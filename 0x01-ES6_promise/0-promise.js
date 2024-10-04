33afc6ffb7629a29e966e342b8ef69e4f88b73e1
DYung26 <theadstore1407@gmail.com>
2024-10-03 20:25:30 -0700

    Returns a `Promise` using this prototype function `getResponseFromAPI()`

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -4,6 +4,12 @@ function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       const success = true;
+
+      if (success) {
+        resolve('Data fetched successfully!');
+      } else {
+        reject('Error failed to fetch data.');
+      }
     }, 0);
   });
 }
