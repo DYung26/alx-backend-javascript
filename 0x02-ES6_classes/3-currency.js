bd6038ec2cde7da94b207b5fc626b400e7bb037f
DYung26 <theadstore1407@gmail.com>
2024-10-04 22:04:08 -0700

    a class named `Currency`: - Constructor attributes:   - `code` (String)   - `name` (String) - Implement a getter and setter for each attribute. Implement a method named displayFullCurrency that will return the attributes in the following format name (code).

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,26 @@
+export defaukt class Currency {
+  constructor (code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${code})`;
+  }
+}
