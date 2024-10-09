dbd85f5c3c8ace40cb930982f0ef9ff5e633fd2a
DYung26 <theadstore1407@gmail.com>
2024-10-08 20:52:12 -0700

    Add `JSDoc` comments to `getStudentIdsSum` function for clear parameter and return type definition

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
 
+/**
+ * Retrieves the sum of ids of a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author DYung26 <https://github.com/DYung26>
+ * @returns {Number}
+ */
 export default function getStudentIdsSum(studentsList) {
   if (studentsList instanceof Array) {
     return students.reduce(
