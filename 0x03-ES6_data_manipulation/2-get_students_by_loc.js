12ab9220dc7909ff7d5eaf3813dce2a5e4b58fdc
DYung26 <theadstore1407@gmail.com>
2024-10-08 21:35:59 -0700

    Fix typo in parameter name and correct filter logic in `getStudentsByLocation` function

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,14 +6,14 @@
  *   id: Number,
  *   firstName: String,
  *   location: String
- * }[]} students - The list of students.
+ * }[]} studenstList - The list of students.
  * @param {String} city - The location.
  * @author DYung26 <https://github.com/DYung26>
  * @returns
  */
 export default function getStudentsByLocation(studentsList, city) {
   if (studentsList instanceof Array) {
-    return students.filter((studentsList) => studentsList.location === city);
+    return studentsList.filter((student) => student.location === city);
   }
   return [];
 }
