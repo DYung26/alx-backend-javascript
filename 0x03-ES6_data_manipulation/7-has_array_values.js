854d9e6987ad4adf85d3caec7a58a19dcdcc3b14
DYung26 <theadstore1407@gmail.com>
2024-10-08 20:39:56 -0700

    `More set data structure`

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+/**
+ * Checks if a set contains each element in an array.
+ * @param {Set} set - The collection of unique items.
+ * @param {*} array - The array of items.
+ * @author DYung26 <https://github.com/DYung26>
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
