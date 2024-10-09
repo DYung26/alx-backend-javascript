71eee0d3da417886a9225d20539b83263435399f
DYung26 <theadstore1407@gmail.com>
2024-10-08 20:37:45 -0700

    `Set data structure`

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author DYung26 <https://github.com/DYung26>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
