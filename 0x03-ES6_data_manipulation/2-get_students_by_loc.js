1dcf93a3bd21666a7ee8f6d12906c908d2cb6f13
DYung26 <theadstore1407@gmail.com>
2024-10-08 20:51:43 -0700

    Document `getStudentsByLocation` function using `JSDoc` to specify parameters and return value

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,5 +1,16 @@
 #!/usr/bin/node
 
+/**
+ * Retrieves students in a given location.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @param {String} city - The location.
+ * @author DYung26 <https://github.com/DYung26>
+ * @returns
+ */
 export default function getStudentsByLocation(studentsList, city) {
   if (studentsList instanceof Array) {
     return students.filter((studentsList) => studentsList.location === city);
