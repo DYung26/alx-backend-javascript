8477a9c841ff3c2bc590634c9376491898701bb2
DYung26 <theadstore1407@gmail.com>
2024-10-08 20:51:21 -0700

    Enhance `getListStudentIds` function with `JSDoc` comments to describe parameters and return type

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,15 @@
 #!/usr/bin/node
 
+/**
+ * Retrieves ids from a list of students.
+ * @param {{
+ *   id: Number,
+ *   firstName: String,
+ *   location: String
+ * }[]} students - The list of students.
+ * @author DYung26 <https://github.com/DYung26>
+ * @returns
+ */
 export default function getListStudentIds(objectsList) {
   if (objectsList instanceof Array) {
     return objectsList.map((object) => object.id);
