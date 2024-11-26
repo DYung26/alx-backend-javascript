ceeb68c1cefed5e605122683a1ebba6542c89f1d
DYung26 <theadstore1407@gmail.com>
2024-11-27 00:35:42 +0100

    Add null check to handle cases where `process.stdin.read` returns null

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -6,7 +6,9 @@ process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
-  process.stdout.write(`Your name is: ${name}`);
+  if (name) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
