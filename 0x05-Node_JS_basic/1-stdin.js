3c53abf0e431d9e5d2dedf4801f13b9c1ad8d69a
DYung26 <theadstore1407@gmail.com>
2024-11-27 00:27:58 +0100

    Replace `console.log` with `process.stdout.write` for consistent output formatting

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-console.log('Welcome to Holberton School, what is your name?\n')
+process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
