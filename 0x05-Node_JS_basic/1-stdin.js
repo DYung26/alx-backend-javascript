63f2ca82289a7ca95a28df7f4367b8f28da2c0aa
DYung26 <theadstore1407@gmail.com>
2024-11-27 00:38:50 +0100

    Remove redundant `process.stdin.resume()` and `setEncoding()` calls for streamlined code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,6 @@
 
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
-process.stdin.resume();
-process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
