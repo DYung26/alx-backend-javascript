a2a368bb92edf6ae7d2a594a356c7c9fdda06f65
DYung26 <theadstore1407@gmail.com>
2024-11-27 03:01:48 +0100

    remove `Node.js` interpreter

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
-#!/usr/bin/node
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n')
 
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
 process.stdin.on('readable', () => {
   const name = process.stdin.read();
   if (name) {
