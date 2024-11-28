80b2b8eb214ce013edc28579a0ab7b50923bf7de
DYung26 <theadstore1407@gmail.com>
2024-11-28 20:15:01 +0100

    `test`: fix typo in `SUBTRACT` type and add new test cases for `calculateNumber` function

--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -4,10 +4,16 @@ const assert = require("assert");
 describe('calculateNumber', function () {
   const testCases = [
     { a: 1, b: 3, type: "SUM", expected: 4 },
-    { a: 2, b: -3, type: "SUBTRACT", expected: -5 },
+    { a: 54, b: 3, type: "SUBTRACCT", expected: 52 },
+    { a: 51, b: 27, type: "DIVIDE", expected: 3 },
+    { a: 54, b: -43, type: "SUM", expected: 11 },
+    { a: 2, b: -3, type: "SUBTRACT", expected: 5 },
+    { a: 81, b: -9, type: "DIVIDE", expected: -9 },
+    { a: -65, b: 5, type: "SUM", expected: -60 },
     { a: -15, b: 5, type: "DIVIDE", expected: -3 },
     { a: -7, b: -4, type: "SUBTRACT", expected: 3 },
     { a: 1.2, b: 4, type: "SUM", expected: 5 },
+    { a: 1.4, 4.5, type: "SUM", expected: 6 },
     { a: 8, b: 0, type: "DIVIDE", expected: "Error" },
   ];
 
