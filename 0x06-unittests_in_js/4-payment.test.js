24612f84195af9f1be76e4117d2bafe77e2b6161
DYung26 <theadstore1407@gmail.com>
2024-11-29 05:51:35 +0100

    `Test sendPaymentRequestToApi`: Stub `calculateNumber` and verify `console.log` output

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,23 @@
+const sinon = require('sinon');
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
+const { expect } = require('chai');
+
+describe('sendPaymentRequestToApi', function () {
+  it('...', function () {
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    // const spy = sinon.spy(Utils, 'calculateNumber');
+    const logSpy = sinon.spy(console, 'log');
+    const result = sendPaymentRequestToApi(100, 20);
+
+    expect(logSpy.calledWith('The total is: 10')).to.be.true;
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.returned(10)).to.be.true;
+    expect(result).to.equal(10);
+
+    // spy.restore();
+    stub.restore();
+  });
+});
+
